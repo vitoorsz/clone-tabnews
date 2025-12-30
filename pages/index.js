1467ce6087a9ae1228edff003e1fd47b11b4101d
vitoorsz <120274958+vitoorsz@users.noreply.github.com>
2025-12-30 04:53:29 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Samara, eu amo você, se você me ama, dá uma risadinha!</h1>
+    return <h1>Samara, eu amo você, se você me ama, dá uma risadinha! 😎 </h1>
 }
 
 export default Home;
